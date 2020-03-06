3fd9896e3fc1cd90b3425d1503cbea37ed03d551
Jan Smutný <jan.smutny@pslib.cz>
2020-03-06 09:45:30 +0100

    Add project files.

---
+++ RPS/Model/Choice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPS.Model
+{
+    public enum Choice
+    {
+        None,
+        Rock,
+        Paper,
+        Scissors,
+    }
+}
